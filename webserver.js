aaf7167f69b7643f91b3f29c456c2932a33208a1
coyotte508 <coyotte508@gmail.com>
2015-09-20 01:06:53 +0200

    Add some kiwi functionality

--- webserver.js
+++ webserver.js
@@ -6,11 +6,11 @@ console.log(__dirname + '/public');
 app.use("/public", express.static(__dirname + '/public'));
 
 app.get("/", function(req, res) {
-    res.render("index.kiwi");
+    res.render("index.kiwi", {param:"value"});
 });
 
 app.get("/admin", function(req, res) {
-    res.render("admin.kiwi");
+    res.render("admin.kiwi", {bannedips:["666.666.666.666", "12.34.56.78"]});
 });
 
 app.listen(1234);
