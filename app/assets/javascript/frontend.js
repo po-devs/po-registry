27bde09c8a41b2b3cad3b4120ff72c768ab90512
coyotte508 <coyotte508@gmail.com>
2015-09-24 19:00:46 +0200

    Connect to the webclient from the registry

--- app/assets/javascript/frontend.js
+++ app/assets/javascript/frontend.js
@@ -14,4 +14,23 @@ $(function() {
       data: {"ip": $(this).attr('ip')}
     });
   });
+
+  $(document).on("click", ".connect-btn", function(event) {
+    $('#ip').val($(this).attr('ip'));
+    $('#port').val($(this).attr('port'));
+
+    submitConnectForm();
+  });
+
+  $(document).on("click", "#main-connect-button", function(event) {
+    submitConnectForm();
+    return false;
+  });
 });
+
+function submitConnectForm() {
+  $("#connect-form").attr("action", $("#connect-form").attr("action") + "?relay=" + $("#relay").val() + "&port=" + $("#port").val()
+    + "&user=" + $("#user").val());
+
+  document.forms["connect"].submit();
+}
