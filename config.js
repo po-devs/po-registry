ec632dc6d0ee3d751fb8ab95df728331b9a70a57
coyotte508 <coyotte508@gmail.com>
2015-10-06 11:28:28 +0200

    Change webclient url

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ config.antispam = {};
 config.web.port = 1234;
 config.web.user = "admin";
 config.web.password = "admin";
-config.web.webclient = "http://webclient.pokemon-online.eu/";
+config.web.webclient = "http://web.pokemon-online.eu/";
 
 config.antispam.logins = 10;
 config.antispam.requests = 40;
