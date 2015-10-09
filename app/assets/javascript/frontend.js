371ee4ac36daace8886eaf23481a1932280e1fcc
coyotte508 <coyotte508@gmail.com>
2015-10-09 13:58:06 +0200

    Show server description on hover

--- app/assets/javascript/frontend.js
+++ app/assets/javascript/frontend.js
@@ -25,7 +25,14 @@ $(function() {
   $("#connect-form").submit(function(event) {
     event.preventDefault();
     submitConnectForm();
-  })
+  });
+
+  $(".server").hover(function(event) {
+    var desc = $(this).attr("description");
+    console.log("hovered, desc:" + desc);
+    //$(".server-description").contents().find("body").html(desc);
+    $(".server-description")[0].src = "data:text/html;charset=utf-8,"+desc;
+  });
 });
 
 function submitConnectForm() {
